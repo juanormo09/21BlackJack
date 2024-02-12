83db49c6695487fb94377ee533c38cbd9f7cd958
juanormo <juanormo7@gmail.com>
2024-02-12 10:55:41 -0500

    Turno del jugador y el dealer

--- 21blackjack.py
+++ 21blackjack.py
@@ -29,3 +29,48 @@ def calcular_puntos(mano):
         ases -= 1
         
     return puntos
+
+def mostrar_mano(mano, oculta = False):
+    if oculta:
+        print(f"Cartas del dealer: [{mano[1]['valor']} de {mano[1]['carta']}] y [Carta Oculta]")
+    else:
+        print("Cartas:")
+        for carta in mano:
+            print(f"[{carta['valor']} de {carta['carta']}]")
+
+def blackjack():
+    mazo = mazo()
+    mano_jugador = [mazo.pop(), mazo.pop()]
+    mano_dealer = [mazo.pop(), mazo.pop()]
+    
+    #Turno del Jugador
+    while True:
+        mostrar_mano(mano_jugador)
+        puntuacion_juagor = calcular_puntos(mano_jugador)
+        print(f"Puntuación Jugador: {puntuacion_juagor}")
+        
+        if puntuacion_juagor == 21:
+            print("BlackJack ! Has ganado.")
+            break
+        elif puntuacion_juagor > 21:
+            print("Perdiste!! te has pasado de 21!!")
+            break
+        
+        opcion = input("¿Quieres tomar otra carta? (s/n): ").lower()
+        if opcion == 's':
+            mano_jugador.append(mazo.pop())
+        else:
+            break
+        
+    
+    # Turno del Dealer
+    mostrar_mano(mano_dealer, oculta = True) 
+    puntuacion_dealer = calcular_puntos(mano_dealer)
+    
+    while puntuacion_dealer < 17:
+        print("El dealer toma otra carta.")
+        mano_dealer.append(mazo.pop())
+        puntuacion_dealer = calcular_puntos(mano_dealer)
+        
+    print("\n Puntuacion del dealer: ", puntuacion_dealer)
+    mostrar_mano(mano_dealer)
